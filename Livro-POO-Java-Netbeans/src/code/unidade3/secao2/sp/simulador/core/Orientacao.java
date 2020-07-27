5a78407794ab78a3b9aad4f8680a102a81699763
Jesimar Arantes <jesimar.arantes@gmail.com>
2020-07-26 21:19:13 -0300

    Atualização U3S2

---
+++ Livro-POO-Java-Netbeans/src/code/unidade3/secao2/sp/simulador/core/Orientacao.java
@@ -0,0 +1,9 @@
+package code.unidade3.secao2.sp.simulador.core;
+
+/**
+ * Enumeração que modela a orientação do robô.
+ * @author Jesimar S. Arantes
+ */
+public enum Orientacao {
+    FRENTE, ATRAS, ESQUERDA, DIREITA;
+}
